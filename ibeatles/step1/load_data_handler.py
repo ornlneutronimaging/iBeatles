22f127733db887a80c04d7689f8fcefa3b2e4b99
JeanBilheux <bilheuxjm@ornl.gov>
2016-09-12 14:50:29 -0400

    Started to try to implement the load file and folder..multi selection.. no success so far

--- ibeatles/step1/load_data_handler.py
+++ ibeatles/step1/load_data_handler.py
@@ -1,3 +1,7 @@
+import PyQt4.QtGui as QtGui
+import sys
+
+
 class LoadDataHandler(object):
     
 
@@ -9,6 +13,18 @@ class LoadDataHandler(object):
         """
         type = ['sample', 'ob', 'normalized', 'time_spectra']
         """
-        pass
+        
+        mydialog = FileDialog()
+        mydialog.setFileMode(QtGui.QFileDialog.Directory)
+        mydialog.setFileMode(QtGui.QFileDialog.AnyFile)
+        filelist = mydialog.getExistingDirectory(self.parent, "select stuff", ".")
         
         
+        
+class FileDialog(QtGui.QFileDialog):
+    def __init__(self, *args):
+        QtGui.QFileDialog.__init__(self, *args)
+        self.setOption(self.DontUseNativeDialog, True)
+        self.setFileMode(self.ExistingFiles)
+
+
