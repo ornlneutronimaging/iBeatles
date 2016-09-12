a36b94d0c0d97c61b773d5878164a99a683f2b6b
JeanBilheux <bilheuxjm@ornl.gov>
2016-09-12 09:51:46 -0400

    Started to implement the loading of the raw data

---
+++ ibeatles/step1/load_data_handler.py
@@ -0,0 +1,14 @@
+class LoadDataHandler(object):
+    
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        
+    
+    def load(self, type='sample'):
+        """
+        type = ['sample', 'ob', 'normalized', 'time_spectra']
+        """
+        pass
+        
+        
