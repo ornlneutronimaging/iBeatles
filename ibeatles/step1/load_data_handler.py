6e94f4790e5cccb9ea15807e6e559158afb1b3d8
JeanBilheux <bilheuxjm@ornl.gov>
2016-09-13 09:53:24 -0400

    Loading multi files and folder now works

--- ibeatles/step1/load_data_handler.py
+++ ibeatles/step1/load_data_handler.py
@@ -1,6 +1,6 @@
 import PyQt4.QtGui as QtGui
 import sys
-
+import os
 
 class LoadDataHandler(object):
     
@@ -15,16 +15,28 @@ class LoadDataHandler(object):
         """
         
         mydialog = FileDialog()
-        mydialog.setFileMode(QtGui.QFileDialog.Directory)
-        mydialog.setFileMode(QtGui.QFileDialog.AnyFile)
-        filelist = mydialog.getExistingDirectory(self.parent, "select stuff", ".")
-        
-        
+        mydialog.exec_()
+
         
 class FileDialog(QtGui.QFileDialog):
     def __init__(self, *args):
         QtGui.QFileDialog.__init__(self, *args)
         self.setOption(self.DontUseNativeDialog, True)
         self.setFileMode(self.ExistingFiles)
+        btns = self.findChildren(QtGui.QPushButton)
+        self.openBtn = [x for x in btns if 'open' in str(x.text()).lower()][0]
+        self.openBtn.clicked.disconnect()
+        self.openBtn.clicked.connect(self.openClicked)
+        self.tree = self.findChild(QtGui.QTreeView)
 
+    def openClicked(self):
+        inds = self.tree.selectionModel().selectedIndexes()
+        files = []
+        for i in inds:
+            if i.column() == 0:
+                files.append(os.path.join(str(self.directory().absolutePath()),str(i.data().toString())))
+        self.selectedFiles = files
+        self.hide()
 
+    def filesSelected(self):
+        return self.selectedFiles
