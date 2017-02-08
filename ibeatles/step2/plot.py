Fixed linear region of normalization tab

ibeatles/step2/plot.py
@@ -123,11 +123,11 @@ class Step2Plot(object):
             x_axis = np.arange(len(_array_sample_vs_file_index))
             curve = _plot_ui.plot(_array_sample_vs_file_index)
             _plot_ui.setLabel("bottom", "File Index")
-            if self.parent.range_files_to_normalized_step2['file_index'] == []:
-                _range_files_to_normalized_step2 = [0, x_axis[-1]]
-                self.parent.range_files_to_normalized_step2['file_index'] = _range_files_to_normalized_step2
-            else:
-                _range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['file_index']
+            #if self.parent.range_files_to_normalized_step2['file_index'] == []:
+                #_range_files_to_normalized_step2 = [0, x_axis[-1]]
+                #self.parent.range_files_to_normalized_step2['file_index'] = _range_files_to_normalized_step2
+            #else:
+                #_range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['file_index']
 
         elif xaxis_choice == 'tof':
             tof_array = self.parent.data_metadata['time_spectra']['data']
@@ -135,11 +135,11 @@ class Step2Plot(object):
             curve = _plot_ui.plot(tof_array, _array_sample_vs_file_index)
             _plot_ui.setLabel("bottom", u"TOF (\u00B5s)")
             x_axis = tof_array
-            if self.parent.range_files_to_normalized_step2['tof'] == []:
-                _range_files_to_normalized_step2 = [tof_array[0], tof_array[-1]]
-                self.parent.range_files_to_normalized_step2['tof'] = _range_files_to_normalized_step2 
-            else:
-                _range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['tof']
+            #if self.parent.range_files_to_normalized_step2['tof'] == []:
+                #_range_files_to_normalized_step2 = [tof_array[0], tof_array[-1]]
+                #self.parent.range_files_to_normalized_step2['tof'] = _range_files_to_normalized_step2 
+            #else:
+                #_range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['tof']
 
         else:
             lambda_array = self.parent.data_metadata['time_spectra']['lambda']
@@ -148,20 +148,32 @@ class Step2Plot(object):
             _plot_ui.setLabel("bottom", u'\u03BB (\u212B)')
             x_axis = lambda_array
             
-            if self.parent.range_files_to_normalized_step2['lambda'] == []:
-                _range_files_to_normalized_step2 = [lambda_array[0], lambda_array[-1]]
-                self.parent.range_files_to_normalized_step2['lambda'] = _range_files_to_normalized_step2 
-            else:
-                _range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['lambda']
+            #if self.parent.range_files_to_normalized_step2['lambda'] == []:
+                #_range_files_to_normalized_step2 = [lambda_array[0], lambda_array[-1]]
+                #self.parent.range_files_to_normalized_step2['lambda'] = _range_files_to_normalized_step2 
+            #else:
+                #_range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['lambda']
+
+        if self.parent.range_files_to_normalized_step2['file_index'] == []:
+            _range_files_to_normalized_step2 = [0, x_axis[-1]]
+            self.parent.range_files_to_normalized_step2['file_index'] = _range_files_to_normalized_step2
+        else:
+            _range_files_to_normalized_step2 = self.parent.range_files_to_normalized_step2['file_index']
             
         # labels
         _plot_ui.setLabel("left", "ROI Mean Counts")
         
         # display range of file to keep
         
-        lr = pg.LinearRegionItem(values=_range_files_to_normalized_step2, orientation=None, brush=None, 
-                                movable=True, 
-                                bounds=None)
+        linear_region_range = [x_axis[_range_files_to_normalized_step2[0]],
+                              x_axis[_range_files_to_normalized_step2[1]]]
+
+        lr = pg.LinearRegionItem(values=linear_region_range, 
+                                 orientation=None, 
+                                 brush=None, 
+                                 movable=True, 
+                                 bounds=None)
+
         lr.sigRegionChangeFinished.connect(self.parent.step2_bragg_edge_selection_changed)
         lr.setZValue(-10)
         self.parent.step2_ui['bragg_edge_plot'].addItem(lr)