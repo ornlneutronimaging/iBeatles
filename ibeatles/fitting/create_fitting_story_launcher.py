a710891ccfd184257c6531fb80b6d48d6cc9cfdd
JeanBilheux <bilheuxjm@ornl.gov>
2017-03-20 11:59:31 -0400

    Added docs of how to initialize the variables (sent by Gian) and implemented the init story table

--- ibeatles/fitting/create_fitting_story_launcher.py
+++ ibeatles/fitting/create_fitting_story_launcher.py
@@ -49,10 +49,11 @@ class FittingStoryWindow(QMainWindow):
         QMainWindow.__init__(self, parent=parent)
         self.ui= UiMainWindow()
         self.ui.setupUi(self)
+        self.fill_table()
         
     def clear_table(self):
         nbr_row = self.ui.story_table.rowCount()
-        for _row in nbr_row:
+        for _row in np.arange(nbr_row):
             self.ui.story_table.removeRow(0)
         
     def fill_table(self):
@@ -76,24 +77,30 @@ class FittingStoryWindow(QMainWindow):
             elif _status_of_row == Status.completed:
                 _color = QtGui.QColor(0, 0, 255, alpha=255)
             else:
-                _color = QtGui.QColor(255, 255, 255)
+                _color = QtGui.QColor(0, 0, 0)
             
-            _item = self.set_item(self, text=str(_index+1))
-            _item.setTextColor(_color)
-            story_table.setItem(_index, 0, _item)
+            #_item = self.set_item(text=str(_index+1))
+            #_item.setTextColor(_color)
+            #story_table.setItem(_index, 0, _item)
                         
             for _index_tag, _tag in enumerate(self.list_column_tag):
                 _widget = self.set_widget(status=_entry[_tag])
-                story_table.setCellWidget(_index, _index_tag + 1, _widget)
+                story_table.setCellWidget(_index, _index_tag, _widget)
 
     def set_item(self, text=''):
         _item = QtGui.QTableWidgetItem(text)
         return _item
 
     def set_widget(self, status=False):
+        _layout = QtGui.QHBoxLayout()
         _widget = QtGui.QCheckBox()
         _widget.setChecked(status)
-        return _widget
+        _layout.addStretch()
+        _layout.addWidget(_widget)
+        _layout.addStretch()
+        _new_widget = QtGui.QWidget()
+        _new_widget.setLayout(_layout)
+        return _new_widget
         
     def add_row_button_clicked(self):
         pass
--- ibeatles/table_dictionary/table_fitting_story_dictionary_handler.py
+++ ibeatles/table_dictionary/table_fitting_story_dictionary_handler.py
@@ -1,22 +1,48 @@
+try:
+    import PyQt4.QtGui as QtGui
+    import PyQt4.QtCore as QtCore
+    from PyQt4.QtGui import QApplication 
+except:
+    import PyQt5.QtGui as QtGui
+    import PyQt5.QtCore as QtCore
+    from PyQt5.QtWidgets import QApplication
+import numpy as np
+
 from ibeatles.utilities.status import Status
 
 
 class TableFittingStoryDictionaryHandler(object):
     
+    story_1 = [['a1','a6'],
+               ['a2','a5'],
+               ['d_spacing','sigma','alpha'],
+               ['a1','a2','a5','a6'],
+               ['d_spacing','sigma','alpha'],
+               ['d_spacing','sigma','alpha','a1','a2','a5','a6']]
+
+    list_widget_tag = ['d_spacing','sigma','alpha','a1','a2','a5','a6']
+
     def __init__(self, parent=None):
         self.parent = parent
         
     def initialize_table(self):
+      
         table_fitting_story_dictionary = {}
         
-        table_fitting_story_dictionary[0] = {'d_spacing': False,
-                                             'sigma': False,
-                                             'alpha': False,
-                                             'a1': False,
-                                             'a2': False,
-                                             'a5': False,
-                                             'a6': False,
-                                             'progress': Status.not_run_yet}
+        for row in np.arange(6):
+            table_fitting_story_dictionary[row] = {'d_spacing': False,
+                                                   'sigma': False,
+                                                   'alpha': False,
+                                                   'a1': False,
+                                                   'a2': False,
+                                                   'a5': False,
+                                                   'a6': False,
+                                                   'progress': Status.not_run_yet}
+        
+        for _index, _story in enumerate(self.story_1):
+            for _variable in _story:
+                table_fitting_story_dictionary[_index][_variable] = True
         
         self.parent.table_fitting_story_dictionary = table_fitting_story_dictionary
-        
+
+   
