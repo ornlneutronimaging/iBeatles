5ded9fc25147b73ba5074db8fcefd640248d3a0c
JeanBilheux <bilheuxjm@ornl.gov>
2017-05-02 12:30:05 -0400

    second commit about bringing to life the rotate_images.

---
+++ ibeatles/tools/rotate_images.py
@@ -0,0 +1,55 @@
+try:
+    import PyQt4
+    import PyQt4.QtGui as QtGui
+    import PyQt4.QtCore as QtCore
+    from PyQt4.QtGui import QMainWindow
+    from PyQt4.QtGui import QApplication         
+except:
+    import PyQt5
+    import PyQt5.QtGui as QtGui
+    import PyQt5.QtCore as QtCore
+    from PyQt5.QtWidgets import QMainWindow
+    from PyQt5.QtWidgets import QApplication
+    
+from pyqtgraph.dockarea import *
+import pyqtgraph as pg
+import numpy as np
+
+from ibeatles.interfaces.ui_rotateImages import Ui_MainWindow as UiMainWindow
+    
+    
+class RotateImages(object):
+    
+    def __init__(self, parent=None):
+        self.parent = parent
+        
+        if self.parent.rotate_ui == None:
+            rotate_ui = RotateImagesWindow(parent=parent)
+            rotate_ui.show()
+            self.parent.rotate_ui = rotate_ui
+        else:
+            self.parent.rotate_ui.setFocus()
+            self.parent.rotate_ui.activateWindow()
+        
+        
+class RotateImagesWindow(QMainWindow):
+    
+    def __init__(self, parent=None):
+        
+        self.parent = parent
+        QMainWindow.__init__(self, parent=parent)
+        self.ui = UiMainWindow()
+        self.ui.setupUi(self)
+        
+        self.init_pyqtgraph()
+        
+    def init_pyqtgraph(self):
+        pass
+    
+    def cancel_clicked(self):
+        self.closeEvent(self)
+        
+    def closeEvent(self, event=None):
+        self.parent.rotate_ui.close()
+        self.parent.rotate_ui = None
+        
