b1ce7361aa7043da42145255a85a639684deb628
JeanBilheux <bilheuxjm@ornl.gov>
2022-01-25 14:37:38 -0500

    Handling format error before running fitting. this refs #77

--- src/iBeatles/fitting/kropff/fit_regions.py
+++ src/iBeatles/fitting/kropff/fit_regions.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 from src.iBeatles.utilities.status_message_config import StatusMessageStatus, show_status_message
 import src.iBeatles.utilities.error as fitting_error
@@ -12,6 +13,8 @@ class FitRegions:
         self.grand_parent = grand_parent
 
     def all_regions(self):
+        type_error = ""
+
         try:
             self.high_lambda()
             self.low_lambda()
@@ -23,18 +26,54 @@ class FitRegions:
         except fitting_error.BraggPeakFittingError as err:
             type_error = err
 
-        show_status_message(parent=self.parent,
-                            message=f"Error fitting {type_error}",
-                            status=StatusMessageStatus.error,
-                            duration_s=5)
+        if type_error:
+            show_status_message(parent=self.parent,
+                                message=f"Error fitting {type_error}",
+                                status=StatusMessageStatus.error,
+                                duration_s=5)
 
     def high_lambda(self):
         a0 = self.parent.ui.kropff_high_lda_a0_init.text()
-        raise fitting_error.HighLambdaFittingError(fitting_region=FittingRegions.high_lambda,
-                                                   message="Wrong a0 format!")
+        try:
+            a0 = np.float(a0)
+        except ValueError:
+            raise fitting_error.HighLambdaFittingError(fitting_region=FittingRegions.high_lambda,
+                                                       message=u"Wrong a\u2080 format!")
+
+        b0 = self.parent.ui.kropff_high_lda_b0_init.text()
+        try:
+            b0 = np.float(b0)
+        except ValueError:
+            raise fitting_error.HighLambdaFittingError(fitting_region=FittingRegions.high_lambda,
+                                                       message=u"Wrong b\u2080 format!")
 
     def low_lambda(self):
-        pass
+        ahkl = self.parent.ui.kropff_low_lda_ahkl_init.text()
+        try:
+            ahkl = np.float(ahkl)
+        except ValueError:
+            raise fitting_error.LowLambdaFittingError(fitting_region=FittingRegions.low_lambda,
+                                                      message=u"Wrong a\u2095\u2096\u2097 format!")
+
+        bhkl = self.parent.ui.kropff_low_lda_bhkl_init.text()
+        try:
+            bhkl = np.float(bhkl)
+        except ValueError:
+            raise fitting_error.LowLambdaFittingError(fitting_region=FittingRegions.low_lambda,
+                                                      message="Wrong b\u2095\u2096\u2097 format!")
 
     def bragg_peak(self):
-        pass
+        lambda_hkl = self.parent.ui.kropff_bragg_peak_ldahkl_init.text()
+        try:
+            lambda_hkl = np.float(lambda_hkl)
+        except ValueError:
+            raise fitting_error.BraggPeakFittingError(fitting_region=FittingRegions.bragg_peak,
+                                                      message=u"Wrong \u03BB\u2095\u2096\u2097 format!")
+
+        tau = self.parent.ui.kropff_bragg_peak_tau_init.text()
+        try:
+            tau = np.float(tau)
+        except ValueError:
+            raise fitting_error.BraggPeakFittingError(fitting_region=FittingRegions.bragg_peak,
+                                                      message=u"Wrong \u03c4 format!")
+        
