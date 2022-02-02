f47ee384be49834b527b6eefc5d7d385c026e079
JeanBilheux <bilheuxjm@ornl.gov>
2022-02-02 11:42:34 -0500

    initialization of all labels. this refs #94

--- src/iBeatles/step6/initialization.py
+++ src/iBeatles/step6/initialization.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from src.iBeatles import ANGSTROMS, LAMBDA, SUB_0
 
 
@@ -9,6 +11,7 @@ class Initialization:
 
     def all(self):
         self.labels()
+        self.parameters()
 
     def labels(self):
         self.parent.ui.range_selected_from_units_label.setText(ANGSTROMS)
@@ -18,3 +21,18 @@ class Initialization:
         self.parent.ui.reference_material_lambda0_units_label.setText(ANGSTROMS)
 
         self.parent.ui.reference_material_lambda0_label.setText(LAMBDA + SUB_0)
+
+    def parameters(self):
+        from_lambda = self.grand_parent.fitting_ui.ui.lambda_min_lineEdit.text()
+        to_lambda = self.grand_parent.fitting_ui.ui.lambda_max_lineEdit.text()
+        hkl_selected = self.grand_parent.fitting_ui.ui.hkl_list_ui.currentText()
+        lambda_0 = np.float(self.grand_parent.fitting_ui.ui.bragg_edge_calculated.text())
+        element = self.grand_parent.fitting_ui.ui.material_groupBox.title()
+
+        self.parent.ui.from_lambda.setText(from_lambda)
+        self.parent.ui.to_lambda.setText(to_lambda)
+        self.parent.ui.hkl_value.setText(hkl_selected)
+        self.parent.ui.d0_value.setText("{:.03f}".format(lambda_0 / 2.))
+        self.parent.ui.material_name.setText(element)
+        self.parent.ui.lambda_0.setText("{:.03f}".format(lambda_0))
+        self.parent.ui.d0_user_value.setText("{:.03f}".format(lambda_0 / 2.))
