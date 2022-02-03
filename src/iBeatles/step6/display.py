a5f876ac32f177604684197b2ab519ecfd066765
JeanBilheux <bilheuxjm@ornl.gov>
2022-02-03 09:06:02 -0500

    removed obsolete matplotlib code. this refs #94

--- src/iBeatles/step6/display.py
+++ src/iBeatles/step6/display.py
@@ -1,6 +1,4 @@
 import numpy as np
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from qtpy.QtWidgets import QApplication
 
 from src.iBeatles.step6.get import Get
 
@@ -12,9 +10,8 @@ class Display:
         self.grand_parent = grand_parent
 
     def run(self):
-        return
-        self.parent.strain_mapping_plot.axes.cla()
-        QApplication.processEvents()
+
+        self.parent.image_view.clear()
         if self.parent.ui.display_d_radioButton.isChecked():
             self.d_array()
         elif self.parent.ui.display_strain_mapping_radioButton.isChecked():
@@ -26,47 +23,15 @@ class Display:
 
     def integrated_image(self):
         integrated_image = self.parent.integrated_image
-        img = self.parent.strain_mapping_plot.axes.imshow(integrated_image,
-                                                          interpolation='nearest',
-                                                          vmin=0,
-                                                          vmax=1)
-        img.set_cmap('viridis')
-        self.parent.strain_mapping_plot.axes.axis('off')
-
-        # divider = make_axes_locatable(self.parent.strain_mapping_plot.axes)
-        # cax = divider.append_axes("right", size="5%", pad=0.05)
-        # self.parent.strain_mapping_plot.fig.colorbar(img, cax=cax)
+        self.parent.image_view.setImage(np.transpose(integrated_image))
 
     def d_array(self):
         d_array = self.parent.d_array
-        min_value = np.min(d_array)
-        max_value = np.max(d_array)
-        img = self.parent.strain_mapping_plot.axes.imshow(d_array,
-                                                          interpolation='nearest',
-                                                          vmin=min_value,
-                                                          vmax=max_value)
-        img.set_cmap('viridis')
-        self.parent.strain_mapping_plot.axes.axis('off')
-
-        divider = make_axes_locatable(self.parent.strain_mapping_plot.axes)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
-        self.parent.strain_mapping_plot.fig.colorbar(img, cax=cax)
+        self.parent.image_view.setImage(np.transpose(d_array))
 
     def strain_mapping(self):
         d_array = self.parent.d_array
         o_get = Get(parent=self.parent)
         d0 = o_get.active_d0()
         strain_mapping = (d_array - d0) / d0
-
-        min_value = np.min(strain_mapping)
-        max_value = np.max(strain_mapping)
-        img = self.parent.strain_mapping_plot.axes.imshow(strain_mapping,
-                                                          interpolation='nearest',
-                                                          vmin=min_value,
-                                                          vmax=max_value)
-        img.set_cmap('viridis')
-        self.parent.strain_mapping_plot.axes.axis('off')
-
-        # divider = make_axes_locatable(self.parent.strain_mapping_plot.axes)
-        # cax = divider.append_axes("right", size="5%", pad=0.05)
-        # self.parent.strain_mapping_plot.fig.colorbar(img, cax=cax)
+        self.parent.image_view.setImage(np.transpose(strain_mapping))
--- src/iBeatles/step6/initialization.py
+++ src/iBeatles/step6/initialization.py
@@ -1,12 +1,9 @@
 import numpy as np
 import matplotlib
-matplotlib.use('Qt5Agg')
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from qtpy.QtWidgets import QVBoxLayout
 import pyqtgraph as pg
 
 from src.iBeatles import ANGSTROMS, LAMBDA, SUB_0
-from src.iBeatles.utilities.mplcanvas import MplCanvas
 from src.iBeatles import DataType
 
 
@@ -19,7 +16,6 @@ class Initialization:
     def all(self):
         self.labels()
         self.parameters()
-        # self.matplotlib()
         self.pyqtgraph()
         self.data()
 
@@ -46,21 +42,6 @@ class Initialization:
         self.parent.ui.material_name.setText(element)
         self.parent.ui.lambda_0.setText("{:.03f}".format(lambda_0))
         self.parent.ui.d0_user_value.setText("{:.03f}".format(lambda_0 / 2.))
-
-    def matplotlib(self):
-
-        def _matplotlib(parent=None, widget=None):
-            sc = MplCanvas(parent, width=4, height=2, dpi=300)
-            # sc.axes.plot([0,1,2,3,4,5], [10, 1, 20 ,3, 40, 50])
-            toolbar = NavigationToolbar(sc, parent)
-            layout = QVBoxLayout()
-            layout.addWidget(toolbar)
-            layout.addWidget(sc)
-            widget.setLayout(layout)
-            return sc
-
-        self.parent.strain_mapping_plot = _matplotlib(parent=self.parent,
-                                                      widget=self.parent.ui.strain_mapping_widget)
 
     def data(self):
         live_data = self.grand_parent.data_metadata[DataType.normalized]['data']
