b8cc6a8461b79dc1653c17cf941c0eccc64c1ae5
JeanBilheux <bilheuxjm@ornl.gov>
2024-01-24 07:27:22 -0500

    Exporting and creating the binned data. full image and roi done. Working on repopulating the ui. this refs #218

--- ibeatles/tools/tof_bin/tof_bin_export_launcher.py
+++ ibeatles/tools/tof_bin/tof_bin_export_launcher.py
@@ -1,5 +1,4 @@
-from qtpy.QtWidgets import QDialog
-from qtpy.QtWidgets import QFileDialog
+from qtpy.QtWidgets import QDialog, QApplication, QFileDialog
 import logging
 import os
 import numpy as np
@@ -18,6 +17,13 @@ from ibeatles.tools.utilities import CombineAlgorithm
 
 from ibeatles.utilities.status_message_config import StatusMessageStatus, show_status_message
 from ibeatles.tools.tof_bin.utilities.get import Get
+
+
+class ExportDataType:
+    """Type of image to export, either the full image or only the roi selected"""
+
+    full_image = "full_image"
+    roi = "roi"
 
 
 class TofBinExportLauncher(QDialog):
@@ -52,7 +58,7 @@ class TofBinExportLauncher(QDialog):
 
         return False
 
-    def bin_and_export(self, output_folder=None, data_type='full_image'):
+    def bin_and_export(self, output_folder=None, data_type=ExportDataType.full_image):
         logging.info(f"binning and exporting {data_type}:")
         FileHandler.make_or_reset_folder(output_folder)
         logging.info(f" -> exported to {output_folder}")
@@ -89,31 +95,25 @@ class TofBinExportLauncher(QDialog):
             output_file_name = os.path.join(output_folder, short_file_name)
 
             # create array of that bin
-            _data_dict = self.extract_data_for_this_bin(list_runs=_bin)
+            _image = self.extract_data_for_this_bin(list_runs=_bin, full_image=(data_type == ExportDataType.full_image))
 
             # full image export
-            full_image = _data_dict['full_image']
-            counts_array_full_image.append(int(np.sum(full_image)))
+            counts_array.append(int(np.sum(_image)))
             o_norm = Normalization()
-            o_norm.load(data=full_image)
-            o_norm.data['sample']['file_name'][0] = os.path.basename(output_file_name_full_image)
-            o_norm.export(folder=output_folder_full_image, data_type='sample', file_type='tiff')
-            logging.info(f" -> exported {output_file_name_full_image}")
-
-            # roi image export
-            roi_image = _data_dict['roi_of_image']
-            counts_array_roi.append(int(np.sum(roi_image)))
-            o_norm = Normalization()
-            o_norm.load(data=roi_image)
-            o_norm.data['sample']['file_name'][0] = os.path.basename(output_file_name_roi)
-            o_norm.export(folder=output_folder_roi, data_type='sample', file_type='tiff')
-            logging.info(f" -> exported {output_file_name_roi}")
+            o_norm.load(data=_image)
+            o_norm.data['sample']['file_name'][0] = os.path.basename(output_file_name)
+            o_norm.export(folder=output_folder, data_type='sample', file_type='tiff')
+            logging.info(f" -> exported {output_file_name}")
 
             file_info_dict[short_file_name] = {'file_index': _bin,
                                                'tof': tof_array[_index],
                                                'lambda': lambda_array[_index]}
             number_of_files_created += 1
             self.parent.eventProgress.setValue(_index + 1)
+            QApplication.processEvents()
+
+        self.parent.eventProgress.setVisible(False)
+    QApplication.processEvents()
 
     def ok_clicked(self):
         working_dir = self.top_parent.session_dict[DataType.sample][SessionSubKeys.current_folder]
@@ -134,92 +134,23 @@ class TofBinExportLauncher(QDialog):
         time_stamp = FileHandler.get_current_timestamp()
 
         if self.ui.full_image_checkBox.isChecked():
-
             output_folder_full_image = os.path.join(_folder, f"{base_folder_name}_full_image_binned_{time_stamp}")
             self.bin_and_export(output_folder=output_folder_full_image,
-                                )
+                                data_type=ExportDataType.full_image)
 
         if self.ui.roi_checkBox.isChecked():
-
             output_folder_roi = os.path.join(_folder, f"{base_folder_name}_roi_binned_{time_stamp}")
             self.bin_and_export(output_folder=output_folder_roi,
-                                )
+                                data_type=ExportDataType.roi)
+
+        # reload if user requested it
+        what_to_reload = self.ui.reload_comboBox.currentText()
+        if not what_to_reload:
+            logging.info("Nothing to reload, we are done here!")
+            self.parent.close()
+            return
 
 
-
-
-        file_info_dict = {}
-        number_of_files_created = 0
-
-        counts_array_full_image = []
-        counts_array_roi = []
-
-        for _index, _bin in enumerate(file_index_array):
-
-            logging.info(f" working with bin#{_index}")
-
-            if len(_bin) == 0:
-                logging.info(f" -> empty bin, skipping!")
-                self.parent.eventProgress.setValue(_index + 1)
-                continue
-
-            short_file_name = f"image_{_index:04d}.tif"
-            output_file_name_full_image = os.path.join(output_folder_full_image, short_file_name)
-            output_file_name_roi = os.path.join(output_folder_roi, short_file_name)
-
-            # create array of that bin
-            _data_dict = self.extract_data_for_this_bin(list_runs=_bin)
-
-            # full image export
-            full_image = _data_dict['full_image']
-            counts_array_full_image.append(int(np.sum(full_image)))
-            o_norm = Normalization()
-            o_norm.load(data=full_image)
-            o_norm.data['sample']['file_name'][0] = os.path.basename(output_file_name_full_image)
-            o_norm.export(folder=output_folder_full_image, data_type='sample', file_type='tiff')
-            logging.info(f" -> exported {output_file_name_full_image}")
-
-            # roi image export
-            roi_image = _data_dict['roi_of_image']
-            counts_array_roi.append(int(np.sum(roi_image)))
-            o_norm = Normalization()
-            o_norm.load(data=roi_image)
-            o_norm.data['sample']['file_name'][0] = os.path.basename(output_file_name_roi)
-            o_norm.export(folder=output_folder_roi, data_type='sample', file_type='tiff')
-            logging.info(f" -> exported {output_file_name_roi}")
-
-            file_info_dict[short_file_name] = {'file_index': _bin,
-                                               'tof': tof_array[_index],
-                                               'lambda': lambda_array[_index]}
-            number_of_files_created += 1
-            self.parent.eventProgress.setValue(_index + 1)
-
-
-        self.parent.eventProgress.setVisible(False)
-
-        # o_get = Get(parent=self)
-        # data_type_selected = o_get.combine_export_mode()
-        # self.close()
-        # output_folder = self.parent.combine_run(data_type_selected=data_type_selected)
-        # if output_folder:
-        #     self.parent.reload_run_in_main_ui(data_type_selected=data_type_selected,
-        #                                       output_folder=output_folder)
-        #     self.parent.close()
-        #
-        #     message = f"TOF combined exported to {output_folder}"
-        #     if not (data_type_selected == DataType.none):
-        #         message += f" and loaded back in {data_type_selected}"
-        #     message += "!"
-        #     status = StatusMessageStatus.ready
-        #
-        # else:
-        #     message = "User cancel export process!"
-        #     status = StatusMessageStatus.warning
-        #
-        # show_status_message(parent=self.grand_parent,
-        #                     message=message,
-        #                     status=status,
-        #                     duration_s=10)
 
     def extract_data_for_this_bin(self, list_runs=None, full_image=True):
         """
@@ -254,5 +185,4 @@ class TofBinExportLauncher(QDialog):
         else:
             raise NotImplementedError("this method of adding the binned images is not supported!")
 
-        return {'full_image': image_to_export,
-                }
+        return image_to_export
