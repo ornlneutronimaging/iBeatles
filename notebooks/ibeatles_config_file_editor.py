de0a5a39ce0c9d86e58275d19bb6b3baafa5e804
JeanBilheux <bilheuxjm@ornl.gov>
2025-03-17 16:58:26 -0400

    fixed traispace issues. this ref #246

--- notebooks/ibeatles_config_file_editor.py
+++ notebooks/ibeatles_config_file_editor.py
@@ -1,12 +1,17 @@
 import marimo
 
-__generated_with = "0.11.17"
+__generated_with = "0.10.9"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     import marimo as mo
+    import datetime
+    import json
+    import os
+
+    TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
 
     mo.Html("<font color=blue size=10>iBeatles config file editor</font>")
 
@@ -15,7 +20,7 @@ def _():
         initial_path = "~/SNS/SNAP/IPTS-27829/"
     else:
         initial_path = "/SNS/VENUS/"
-    return DEBUG, initial_path, mo
+    return DEBUG, TIMESTAMP_FORMAT, datetime, initial_path, json, mo, os
 
 
 @app.cell
@@ -34,15 +39,11 @@ def _(config_file, mo):
     else:
         _text = ""
     mo.md(_text)
-
     return
 
 
 @app.cell
-def _(config_file):
-    import json
-    import os
-
+def _(config_file, json, os):
     data = None
     if len(config_file.value) > 0:
         if os.path.exists(config_file.value[0].path):
@@ -50,7 +51,7 @@ def _(config_file):
                 data = json.load(json_file)
 
     data
-    return data, json, json_file, os
+    return data, json_file
 
 
 @app.cell
@@ -58,19 +59,12 @@ def _(data, mo):
     mo.vstack(
         [
             mo.md("##raw data dir"),
-            mo.hstack(
-                [
-                    mo.ui.text(
+            mo.ui.text(
                         value=data["raw_data"]["raw_data_dir"],
                         full_width=True,
                     ),
-                    mo.ui.button(label="...", full_width=False),
-                ],
-                widths=[10, 1],
-            ),
         ]
     )
-
     return
 
 
@@ -79,16 +73,10 @@ def _(data, mo):
     mo.vstack(
         [
             mo.md("##open beam dir"),
-            mo.hstack(
-                [
-                    mo.ui.text(
+            mo.ui.text(
                         value=data["open_beam"]["open_beam_data_dir"],
                         full_width=True,
                     ),
-                    mo.ui.button(label="...", full_width=False),
-                ],
-                widths=[10, 1],
-            ),
         ]
     )
     return
@@ -115,11 +103,13 @@ def _(data, mo):
                     mo.md(f"ROI #{_index}"),
                     mo.vstack(
                         [
-                            mo.ui.text(value=str(_x0), full_width=False, label="x0"),
+                            mo.ui.text(value=str(_x0),
+                                       full_width=False,
+                                       label=r"x<sub>0</sub>"),
                             mo.ui.text(
                                 value=str(_y0),
                                 full_width=False,
-                                label="y0",
+                                label=r"y<sub>0</sub>",
                             ),
                             mo.ui.text(
                                 value=str(_width),
@@ -166,11 +156,11 @@ def _(data, mo):
     _y0 = str(_bins["y0"])
     _width = str(_bins["width"])
     _height = str(_bins["height"])
-    _size = str(_bins["bins_size"])
+    _size = str(_bins["bin_size"])
     mo.vstack(
         [
-            mo.ui.text(value=_x0, label="x0"),
-            mo.ui.text(value=_y0, label="y0"),
+            mo.ui.text(value=_x0, label=r"x<sub>0</sub>"),
+            mo.ui.text(value=_y0, label=r"y<sub>0</sub>"),
             mo.ui.text(value=_width, label="width"),
             mo.ui.text(value=_height, label="height"),
             mo.ui.text(value=_size, label="Bin size"),
@@ -192,11 +182,10 @@ def _(data, mo):
     _lambda_max = f"{_fitting['lambda_max']:.5e}"
     mo.vstack(
         [
-            mo.ui.text(value=_lambda_min, label="Lambda min"),
-            mo.ui.text(value=_lambda_max, label="Lambda max"),
+            mo.ui.text(value=f"{float(_lambda_min) * 1e10: .4f}", label=u"\u03BB" + r"<sub>min</sub>" + " (\u212b)"),
+            mo.ui.text(value=f"{float(_lambda_max) * 1e10: .4f}", label=u"\u03BB" + r"<sub>max</sub>" + " (\u212b)"),
         ]
     )
-
     return
 
 
@@ -210,7 +199,7 @@ def _(mo):
 def _(data, mo):
     _strain = data["analysis"]["strain_mapping"]
     _d0 = str(_strain["d0"])
-    mo.ui.text(value=_d0, label="d0")
+    mo.ui.text(value=_d0, label=u"d\u2080" + f" (\u212b)")
     return
 
 
@@ -229,21 +218,12 @@ def _(data, mo):
                 value=data["analysis"]["distance_source_detector_in_m"],
             ),
             mo.ui.text(
-                label="detector offset (us)",
+                label=u"detector offset (\u03BCs)",
                 value=data["analysis"]["detector_offset_in_us"],
             ),
         ]
     )
     return
-
-
-app._unparsable_cell(
-    r"""
-    \"distance_source_detector_in_m\":\"19.855\"
-    \"detector_offset_in_us\":\"5000\"
-    """,
-    name="_",
-)
 
 
 @app.cell
@@ -276,14 +256,116 @@ def _(data, mo):
     return
 
 
-app._unparsable_cell(
-    r"""
-    \"normalized_data_dir\":\"/Users/j35/SNS/SNAP/IPTS-27829/normalized_03m_10d_2025y_15h_45mn\"
-    \"analysis_results_dir\":\"/Users/j35/SNS/SNAP/IPTS-27829/analysis_03m_10d_2025y_15h_45mn\"
-    \"strain_results_dir\":\"/Users/j35/SNS/SNAP/IPTS-27829/strain_03m_10d_2025y_15h_45mn\"
-    """,
-    name="_",
-)
+@app.cell
+def _(mo):
+    mo.Html("<hr>")
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("""##Repeat analysis for each of the following folders""")
+    return
+
+
+@app.cell
+def _(initial_path, mo):
+    list_sample_folders_ui = mo.ui.file_browser(
+        initial_path=initial_path, multiple=True, selection_mode='directory'
+    )
+    list_sample_folders_ui
+    return (list_sample_folders_ui,)
+
+
+@app.cell
+def _(list_sample_folders_ui, mo):
+    mo.vstack(
+        [
+            mo.Html(f"{len(list_sample_folders_ui.value)} folders (raw sample) have been selected!")
+        ]
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md("## Select where to create the config files & batch script")
+    return
+
+
+@app.cell
+def _(initial_path, mo):
+    output_folder_ui = mo.ui.file_browser(
+        initial_path=initial_path,
+        label="",
+        multiple=False,
+        selection_mode='directory'
+    )
+    output_folder_ui
+    return (output_folder_ui,)
+
+
+@app.cell
+def _(datetime):
+    def get_current_time_in_special_file_name_format():
+        """format the current date and time into something like  04m_07d_2022y_08h_06mn"""
+        current_time = datetime.datetime.now().strftime("%mm_%dd_%Yy_%Hh_%Mmn")
+        return current_time
+    return (get_current_time_in_special_file_name_format,)
+
+
+@app.cell
+def _(json):
+    def save_json(json_file_name, json_dictionary=None):
+        with open(json_file_name, "w") as outfile:
+            json.dump(json_dictionary, outfile)
+    return (save_json,)
+
+
+@app.cell
+def _(
+    data,
+    get_current_time_in_special_file_name_format,
+    list_sample_folders_ui,
+    os,
+    output_folder_ui,
+    save_json,
+):
+    def create_config_files(param):
+        output_folder = output_folder_ui.value
+        list_raw_sample_folders = list_sample_folders_ui.value
+        config_file = data
+        timestamp = get_current_time_in_special_file_name_format()
+
+        for _raw_sample in list_raw_sample_folders:
+            output_filename = os.path.join(output_folder, os.path.basename(_raw_sample), f"_config_{timestamp}")
+            data['raw_data']['raw_data_dir'] = _raw_sample
+
+            save_json(output_filename, json_dictionary=data)
+
+
+    return (create_config_files,)
+
+
+@app.cell
+def _(create_config_files, list_sample_folders_ui, mo):
+    create_config_button = mo.ui.button(label="Create config files and batch script",
+                                        on_click=lambda x: create_config_files(x),
+                                        disabled=len(list_sample_folders_ui.value) == 0,
+                            )
+
+    mo.vstack(
+        [
+            mo.md("#Create config files and batch script"),
+            create_config_button
+        ]
+    )
+    return (create_config_button,)
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
